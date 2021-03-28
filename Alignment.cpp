8471a44b0b35b869816a4e76fa2e7374e50c71cb
ayrapetovai <ayeaye.post@gmail.com>
2021-03-28 03:17:48 +0300

    fix packed structs for windows

--- Alignment.cpp
+++ Alignment.cpp
@@ -3,15 +3,30 @@
 using namespace std;
 
 int main() {
+	// alignof - minimal bytes per field
 	cout << "alignment of char is " << alignof(char) << endl;
 	cout << "alignment of int is " << alignof(int) << endl;
 	cout << "alignment of double is " << alignof(double) << endl;
 	{
-		struct test_struct {
+		struct notpacked_struct {
 			char char_filed;
 			int int_field;
 		};
-		cout << alignof(test_struct) << endl;
-		//cout << alignof(test_struct::int_field) << endl;
+		cout << "alignof notpacked_struct is by " << alignof(notpacked_struct) << " bytes, but size is " << sizeof(notpacked_struct) << endl;
+
+#ifdef _WIN32
+#pragma pack(push,1)
+		struct packed_struct {
+			char char_filed;
+			int int_field;
+		};
+#pragma pack(pop)
+#else
+		struct packed_struct {
+			char char_filed;
+			int int_field;
+		} __attribute__((__packed__));
+#endif
+		cout << "alignof packed_struct is by " << alignof(packed_struct) << " bytes, but size is " << sizeof(packed_struct) << endl;
 	}
 }
