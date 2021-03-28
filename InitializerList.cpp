606653de8afddd80dfca77c5fb2192a80e415de6
artem <ayrapetovai@yandex.ru>
2021-03-28 14:27:00 +0300

    fix build with g++17: vc++17 does not complain about wrong number of arguments for macros

--- InitializerList.cpp
+++ InitializerList.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<complex>
-#include<numbers>
 
 using namespace std;
 
--- Utils.hpp
+++ Utils.hpp
@@ -2,7 +2,7 @@
 #ifndef _MYCXX_UTILS_HPP
 #define _MYCXX_UTILS_HPP
 
-#define WILL_NOT_COMPILE( __code__ ) { /* __code__ */ }
+#define WILL_NOT_COMPILE(__DEAD_CODE__...) {/* __DEAD_CODE__ */}
 
 #define EQUALS_STRING(__VALUE1__, __VALUE2__) ((__VALUE1__ == __VALUE2__)? "==": "!=")
 
