6fb1f46e9dc95de5d81079eb4c4b5a55725dce08
ayrapetovai <ayeaye.post@gmail.com>
2021-03-28 14:29:35 +0300

    windows jokes :-E

--- Utils.hpp
+++ Utils.hpp
@@ -2,7 +2,11 @@
 #ifndef _MYCXX_UTILS_HPP
 #define _MYCXX_UTILS_HPP
 
-#define WILL_NOT_COMPILE(__DEAD_CODE__...) {/* __DEAD_CODE__ */}
+#ifdef _WIN32
+	#define WILL_NOT_COMPILE(__DEAD_CODE__) {/* __DEAD_CODE__ */}
+#else
+	#define WILL_NOT_COMPILE(__DEAD_CODE__...) {/* __DEAD_CODE__ */}
+#endif
 
 #define EQUALS_STRING(__VALUE1__, __VALUE2__) ((__VALUE1__ == __VALUE2__)? "==": "!=")
 
