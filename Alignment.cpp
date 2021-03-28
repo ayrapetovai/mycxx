e352635bae071eb18bb5b19c1e922362fc493f9d
artem <ayrapetovai@yandex.ru>
2021-03-28 03:42:44 +0300

    macro for packing structs

--- Alignment.cpp
+++ Alignment.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+#ifdef _WIN32
+#define PACKED( __struct_declaration_block__ ) __pragma( pack(push, 1) ) __stuact_declaration_block__ __pragma( pack(pop) );
+#else
+#define PACKED( __struct_declaration_block__ ) __struct_declaration_block__ __attribute__((__packed__));
+#endif
+
+// packing can make packed struct to became not portable across different machines and OS
+
 int main() {
 	// alignof - minimal bytes per field
+	// if struct is not packed then alignment equals to the most big field size
 	cout << "alignment of char is " << alignof(char) << endl;
 	cout << "alignment of int is " << alignof(int) << endl;
 	cout << "alignment of double is " << alignof(double) << endl;
@@ -12,21 +21,33 @@ int main() {
 			char char_filed;
 			int int_field;
 		};
-		cout << "alignof notpacked_struct is by " << alignof(notpacked_struct) << " bytes, but size is " << sizeof(notpacked_struct) << endl;
-
-#ifdef _WIN32
-#pragma pack(push,1)
+		cout << "alignof notpacked_struct(char,int) is by " << alignof(notpacked_struct) <<
+			" bytes, but size is " << sizeof(notpacked_struct) << endl;
+	}
+	{
+		struct notpacked_struct {
+			int int_field;
+                        char char_filed;
+                };
+                cout << "alignof notpacked_struct(int, char) is by " << alignof(notpacked_struct) <<
+                        " bytes, but size is " << sizeof(notpacked_struct) << endl; 
+	}
+	{
+		struct notpacked_struct {
+			double double_field;
+			short int_field;
+                        char char_filed;
+                };
+                cout << "alignof notpacked_struct(double, short, char) is by " << alignof(notpacked_struct) <<
+                        " bytes, but size is " << sizeof(notpacked_struct) << endl;
+	}
+	{
+		PACKED(
 		struct packed_struct {
 			char char_filed;
 			int int_field;
-		};
-#pragma pack(pop)
-#else
-		struct packed_struct {
-			char char_filed;
-			int int_field;
-		} __attribute__((__packed__));
-#endif
+		})
 		cout << "alignof packed_struct is by " << alignof(packed_struct) << " bytes, but size is " << sizeof(packed_struct) << endl;
 	}
 }
+
