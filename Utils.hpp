39566927bfa6fa0bfaea0999eabb3b89df150311
ayrapetovai <ayeaye.post@gmail.com>
2021-03-28 14:38:58 +0300

    crossplatform ... as macros argument?

--- Utils.hpp
+++ Utils.hpp
@@ -2,11 +2,13 @@
 #ifndef _MYCXX_UTILS_HPP
 #define _MYCXX_UTILS_HPP
 
-#ifdef _WIN32
-	#define WILL_NOT_COMPILE(__DEAD_CODE__) {/* __DEAD_CODE__ */}
-#else
-	#define WILL_NOT_COMPILE(__DEAD_CODE__...) {/* __DEAD_CODE__ */}
-#endif
+//#ifdef _WIN32
+//	#define WILL_NOT_COMPILE(__DEAD_CODE__) {/* __DEAD_CODE__ */}
+//#else
+//	#define WILL_NOT_COMPILE(__DEAD_CODE__...) {/* __DEAD_CODE__ */}
+//#endif
+ 
+#define  WILL_NOT_COMPILE(...) {}
 
 #define EQUALS_STRING(__VALUE1__, __VALUE2__) ((__VALUE1__ == __VALUE2__)? "==": "!=")
 
