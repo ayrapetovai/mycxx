43b82d61f29a17507ec925ebc1fa4054b45881be
artem <ayrapetovai@yandex.ru>
2021-03-30 15:58:03 +0300

    const pointer

--- Pointer.cpp
+++ Pointer.cpp
@@ -7,6 +7,10 @@ using namespace std;
 // For a type T, T∗ is the type ‘‘pointer to T.’’
 // void* was designed for functions, that are not allowed to do assumptions about type of argument or returning value
 
+// C++ offers two related meanings of ‘‘constant’’:
+// • constexpr: Evaluate at compile time
+// • const: Do not modify in this scope
+
 int main() {
     cout << "size of pointer (void*) is " << sizeof(void*) << " bytes" << endl;
     {
@@ -21,9 +25,11 @@ int main() {
         cout << "&c " << EQUALS_STRING(&c, p) << " p" << endl; // == 
         cout << "address of c is " << (int64_t)&c << ", address in p " << (int64_t)p << endl;
 
-        // TODO find out why abouv code: terminating with uncaught exception of type std::invalid_argument: bitset string ctor has invalid argument
-        // cout << "address of c is " << bitset<sizeof(void*)*8>(&c) << ", address in p " << bitset<sizeof(void*)*8>(p) << endl;
-        // cout << "address of c is " << &c << ", address in p " << p << endl; // TODO why prints values of c in both cases
+        // TODO find out why does above code terminate with uncaught exception of type std::invalid_argument: bitset string ctor has invalid argument
+        RUNTIME_ERROR(
+            cout << "address of c is " << bitset<sizeof(void*)*8>(&c) << ", address in p " << bitset<sizeof(void*)*8>(p) << endl;
+            cout << "address of c is " << &c << ", address in p " << p << endl; // TODO why prints values of c in both cases
+        );
 
         char c2 = *p;
         cout << "type of c2 " << EQUALS_STRING(typeid(c2), typeid(*p)) << " type of *p" << endl; // ==
@@ -82,7 +88,36 @@ int main() {
         int* pi = a;
         COMPILATION_ERROR(
             a = pi; // error: cannot assign pinter to array (based on declaration)
-            // The implicit conversion of the array argument to a pointer means that the size of the array is lost
+            // The implicit conversion of the array variable to a pointer means that the size of the array is lost
         );
+    }
+    {
+        double* a[10];
+        int dist = reinterpret_cast<char*>(a+2) - reinterpret_cast<char*>(a); 
+        cout << "distance in bytes is " << dist << endl; // 16
+    }
+    {
+        // const influicing on a name to it's right.
+        // When using a pointer, two objects are involved: the pointer itself and the object pointed to.
+        // ‘‘Pre- fixing’’ a declaration of a pointer with const makes the object, but not the pointer, a constant.
+        // To declare a pointer itself, rather than the object pointed to, to be a constant, we use the declarator operator ∗const instead of plain ∗.
+        const char *chp = new char[]{ "abc" }; // pinter can be modified, but char string - cannot be modified;
+        COMPILATION_ERROR(
+            chp[0] = 'A';
+        );
+        delete [] chp;
+        chp = new char[]{ "def" }; // ok
+        delete [] chp;
+
+        char * const cp = new char[]{ "abc" }; // pointer cannot be modifier, but value can be modified.
+        cp[0] = 'A';
+        COMPILATION_ERROR(
+            cp = new int[]{ "def" };
+        );
+        delete [] cp;
+
+        // however...
+        char const *p = "abc"; // is a variable pointer to const char, as conat char *p
+        cout << "char const* " << EQUALS_STRING(typeid(char const*), typeid(const char*)) << " const char*" << endl;
     }
 }
