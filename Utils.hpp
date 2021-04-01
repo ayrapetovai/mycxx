80a01cb4badda7320b4e04bf0a3de75c588311a9
artem <ayrapetovai@yandex.ru>
2021-04-02 02:44:51 +0300

    LESS_STRING

--- Utils.hpp
+++ Utils.hpp
@@ -8,4 +8,6 @@
 
 #define EQUALS_STRING(__VALUE1__, __VALUE2__) ((__VALUE1__ == __VALUE2__)? "==": "!=")
 
+#define LESS_STRING(__VALUE1__, __VALUE2__) ((__VALUE1__ < __VALUE2__)? "<": ">=")
+
 #endif // _MYCXX_UTILS_HPP
