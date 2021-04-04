b07acaa8e6b46b682eec50aacd878a578926b6a9
artem <ayrapetovai@yandex.ru>
2021-04-05 02:10:24 +0300

    values of enumerators

--- Enumeration.cpp
+++ Enumeration.cpp
@@ -10,7 +10,8 @@ enum PlainEnumColor {
     blue,
 };
 
-enum class ClassEnumColor {
+enum class ClassEnumColor;  // declaration
+enum class ClassEnumColor { // definition
     red,
     green,
     blue, // ,
@@ -36,12 +37,17 @@ int main() {
         );
     }
     {
-        // enum class with enumerators of type
-        enum class EnumOfShorts: short {
+        // enum class with enumerators of type, underlying type can be integral type only
+        enum class EnumOfShorts: short { // default : int
             one, two
         };
         cout << "EnumOfShorts: short's enumerator's size is " << sizeof(EnumOfShorts::one) << " bytes" << endl; // 2
         cout << "EnumOfShorts's type " << typeid(EnumOfShorts::two).name() << endl; // non-integral type
+        cout << "EnumOfShorts::two is " << (short) EnumOfShorts::two << endl;
+
+        COMPILATION_ERROR(
+            enum class EnueratorWithClassValue: A {} ; //Non-integral type 'A' is an invalid underlying type
+        );
     }
     {
         // warining on forgotten enumerator in switch
@@ -53,6 +59,39 @@ int main() {
             case InSwitch::one: break;
                                 // enumeration value 'two' not handled in switch
         }
+
+    }
+    {
+        // each enumerator has an integer value
+        enum class EnumeratorsWithValue {
+            a = 1,
+            b = 3,
+        };
+        int x = static_cast<int>(EnumeratorsWithValue::a); // extract int value
+    }
+    {
+        // enum cannot be defined the other way it was declared
+        enum class A: char;
+        COMPILATION_ERROR(
+            enum class A: int {}; // Enumeration redeclared with different underlying type 'int' (was 'char') 
+            enum class A {}; // Enumeration redeclared with different underlying type 'int' (was 'char')
+        );
+    }
+    {
+        // A plain enum can be unnamed
+        enum {a = 1, b = 2 };
+        int x = a;
+        int y = b;
+    }
+    {
+        // plain enum can be implicitly converted to bool, enum classes cannot
+        enum PlainEnum { pe };
+        enum class ClassEnum { ce };
+ 
+        if (PlainEnum::pe) {} // if pe is not 0
+        COMPILATION_ERROR(
+            if (ClassEnum::ce) {} // Value of type 'ClassEnum' is not contextually convertible to 'bool'
+        );
     }
 }
 
