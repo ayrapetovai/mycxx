dcc3a60f5dad62bdc1198126ecb07f53f6ea5ef9
artem <ayrapetovai@yandex.ru>
2021-04-06 10:18:11 +0300

    goto

--- Statement.cpp
+++ Statement.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include"Utils.hpp"
 
 using namespace std;
 
@@ -21,6 +22,60 @@ int main() {
 
         // 1. compiler look for methods v.begin() and v.end() in sequence, fields do not count.
         // 2. compiler look for functions begin(v) and end(v) in the enclosing scope.
+    }
+// goto
+    {
+        int x;
+        goto past_assignment;
+        x = 2;
+past_assignment:
+        cout << "goto past assiginment of x, x is (" << x << ")" << endl; // 0
+
+    }
+    {
+        // cannot goto past initialization of variable
+        COMPILATION_ERROR(
+            goto past_initializer1; // jump bypasses variable initialization
+        );
+        int x = 2;
+past_initializer1:
+        cout << "goto past initializer (does not compile) of x is (" << x << ")" << endl;
+    }
+    {
+        // cannot goto past initialization of variable
+        COMPILATION_ERROR(
+            goto past_initializer2;
+        );
+        int y = 1;
+past_initializer2:
+        cout << "goto past initializer, but variable is not used" << endl;
+    }
+    {
+        goto block2_begin;
+        {
+            // int y = 2; // compilation error: goto past initialization of variable (goto block1_begin)
+block1_begin:
+            int x = 2;
+            cout << "can goto betwin csopes" << endl;
+            goto block2_end;
+        }
+        {
+block2_begin:
+            goto block1_begin;
+block2_end:;
+        }
+    }
+    {
+        // nested for loop exit with goto
+        int x = 0;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 1; j++) {
+                x++;
+                goto outer_for_loop_exit;
+            }
+        }
+outer_for_loop_exit:;
+               cout << "Exit outer for loop, iterations were made " << x << endl;
     }
 }
 
