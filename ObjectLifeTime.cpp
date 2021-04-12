e17bcf6cb6b36b4c3081b21d60d937c32c4125da
artem <ayrapetovai@yandex.ru>
2021-04-12 11:25:10 +0300

    object life time control throw pointer/reference/notmal_field

--- ObjectLifeTime.cpp
+++ ObjectLifeTime.cpp
@@ -10,5 +10,56 @@ using namespace std;
 // Thread-local: objects that are declared 'thrad_local' keyword, can be static or automatic.
 
 // TODO make examples
+
+struct Verbose {
+    Verbose() {
+        cout << "Verbose()" << endl;
+    }
+    ~Verbose() {
+        cout << "~Verbose()" << endl;
+    }
+};
+
+
 int main() {
+    {
+        cout << "Reference field is not destructed" << endl;
+        struct A {
+            Verbose& ref_to_verbose;
+            A(Verbose& v) : ref_to_verbose(v) {}
+        };
+        {
+            A a{ *(new Verbose()) };
+        }
+    }
+    {
+        cout << "Non-reference non-pointer field dectructed" << endl;
+        struct B {
+            Verbose verbose;
+            B(Verbose& v) : verbose(v) {}
+        };
+        {
+            B b{ *(new Verbose()) }; // destructor is called
+        }
+    }
+    {
+        cout << "Pointer field's value is not destructed" << endl;
+        struct C {
+            Verbose* ptr_to_verbose;
+            C(Verbose& v) : ptr_to_verbose(&v) {}
+        };
+        {
+            C c{ *(new Verbose()) };
+        }
+    }
+    {
+        cout << "Unique pointer field's value is destroyed" << endl;
+        struct D {
+            unique_ptr<Verbose> uptr_to_verbose;
+            D(Verbose &v) : uptr_to_verbose(&v) {}
+        };
+        {
+            D d{ *(new Verbose()) };
+        }
+    }
 }
