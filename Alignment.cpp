2e344bde1dc2390c6db6042710f565390a32b30c
artem <ayrapetovai@yandex.ru>
2022-12-26 17:44:41 +0300

    alignas

--- Alignment.cpp
+++ Alignment.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<bit>
+#include "Utils.hpp"
 
 using namespace std;
 
@@ -28,29 +30,89 @@ int main() {
     {
         struct notpacked_struct {
             int int_field;
-                        char char_filed;
-                };
-                cout << "alignof notpacked_struct(int, char) is by " << alignof(notpacked_struct) <<
-                        " bytes, but size is " << sizeof(notpacked_struct) << endl; 
+            char char_filed;
+        };
+        cout << "alignof notpacked_struct(int, char) is by " << alignof(notpacked_struct) <<
+            " bytes, but size is " << sizeof(notpacked_struct) << endl;
     }
     {
         struct notpacked_struct {
             double double_field;
-            short int_field;
-                        char char_filed;
-                };
-                cout << "alignof notpacked_struct(double, short, char) is by " << alignof(notpacked_struct) <<
-                        " bytes, but size is " << sizeof(notpacked_struct) << endl;
+            short short_field;
+            char char_filed;
+        };
+        cout << "alignof notpacked_struct(double, short, char) is by " << alignof(notpacked_struct) <<
+            " bytes, but size is " << sizeof(notpacked_struct) << endl;
     }
     {
         PACKED(
-        struct packed_struct {
-            char char_filed;
-            int int_field;
-        })
+            struct packed_struct {
+                char char_filed;
+                int int_field;
+            }
+        )
         cout << "alignof packed_struct is by " << alignof(packed_struct) << " bytes, but size is " << sizeof(packed_struct) << endl;
+    }
+    {
+        // with alignas on struct do nothing?
+        struct notpacked_struct {
+            char char_field1;
+            short short_field;
+            char char_field2;
+        };
 
-        // TODO alignas
+        notpacked_struct ns {1 , 1, 1};
+        long ns_address = reinterpret_cast<long>(&ns);
+        long ns_char_filed1_address = reinterpret_cast<long>(&ns.char_field1);
+        long ns_short_field_address = reinterpret_cast<long>(&ns.short_field);
+        long ns_char_field2_address = reinterpret_cast<long>(&ns.char_field2);
+        cout << "alignof(ans) is " << alignof(ns) << endl;
+        cout << "sizeof(ans) is  " << sizeof(ns) << endl;
+        cout << "ans_char_filed1_address offset: " << (ns_char_filed1_address - ns_address) << endl;
+        cout << "ans_short_field_address offset: " << (ns_short_field_address - ns_address) << endl;
+        cout << "ans_char_filed2_address offset: " << (ns_char_field2_address - ns_address) << endl;
+
+        alignas (long) notpacked_struct ans {1 , 1, 1};
+        long ans_address = reinterpret_cast<long>(&ans);
+        long ans_char_filed1_address = reinterpret_cast<long>(&ans.char_field1);
+        long ans_short_field_address = reinterpret_cast<long>(&ans.short_field);
+        long ans_char_field2_address = reinterpret_cast<long>(&ans.char_field2);
+        cout << "alignof(ans) is " << alignof(ans) << endl;
+        cout << "sizeof(ans) is  " << sizeof(ans) << endl;
+        cout << "ans_char_field1_address offset: " << (ans_char_filed1_address - ans_address) << endl;
+        cout << "ans_short_field_address offset: " << (ans_short_field_address - ans_address) << endl;
+        cout << "ans_char_field2_address offset: " << (ans_char_field2_address - ans_address) << endl;
+    }
+    {
+        cout << "***alignas for fields" << endl;
+
+        struct aligned_struct {
+            alignas (int) unsigned char char_field;
+            alignas (int) unsigned short short_field;
+        } s { 0xFF, 0xFFFF };
+
+        // [0xFF, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00]
+
+        long s_address = reinterpret_cast<long>(&s);
+        long s_char_field_address = reinterpret_cast<long>(&s.char_field);
+        long s_short_field_address = reinterpret_cast<long>(&s.short_field);
+        cout << "alignof(s) is " << alignof(s) << endl;
+        cout << "sizeof(s) is  " << sizeof(s) << endl; // 8
+        cout << "s_char_field_address offset: " << (s_char_field_address - s_address) << endl;
+        cout << "s_short_field_address offset: " << (s_short_field_address - s_address) << endl;
+
+        cout << "s_address < s_char_field_address < s_short_field_address: " << YESNO_STRING(s_address < s_char_field_address < s_short_field_address) << endl;
+
+        // TODO find a way not to use `unsigned long*` in `reinterpret_cast`, but peak the type of pointer automatically
+        static_assert(sizeof(s) == sizeof(unsigned long));
+
+        auto bits = bitset<sizeof(s) * 8>{*reinterpret_cast<unsigned long*>(&s)};
+        if constexpr (endian::native == endian::little) {
+            for (size_t i{}; i < bits.size()/2; i++) {
+                swap(bits[i], bits[bits.size() - i - 1]);
+            }
+        }
+        cout << "bits of `s` are: [" << bits << "], size=" << bits.size() << endl;
     }
 }
 
