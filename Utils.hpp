91b6ea9b50c7a6a4e1aafece135e918d11804b23
artem <ayrapetovai@yandex.ru>
2022-12-26 10:59:18 +0300

    describe_type for type_info.name

--- Utils.hpp
+++ Utils.hpp
@@ -1,3 +1,4 @@
+#include <cxxabi.h> // for demangle's abi::__cxa_demangle
 
 #ifndef _MYCXX_UTILS_HPP
 #define _MYCXX_UTILS_HPP
@@ -11,5 +12,13 @@
 #define LESS_STRING(VALUE1, VALUE2) ((VALUE1 < VALUE2)? "<": ">=")
 
 #define YESNO_STRING(VALUE) (VALUE? "yes": "no")
+
+std::string describe_type(const char* mangled)
+{
+    int status;
+    std::unique_ptr<char[], void (*)(void*)> result(
+            abi::__cxa_demangle(mangled, 0, 0, &status), std::free);
+    return result.get() ? std::string(result.get()) : "error occurred";
+}
 
 #endif // _MYCXX_UTILS_HPP
