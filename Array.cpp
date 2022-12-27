b3b8c05c10bfd7c13eac40d80e8849082dda8976
artem <ayrapetovai@yandex.ru>
2022-12-27 18:31:01 +0300

    for; pointers

--- Array.cpp
+++ Array.cpp
@@ -1,7 +1,17 @@
-#include<iostream>
-#include"Utils.hpp"
+#include <iostream>
+#include <array>
+#include "Utils.hpp"
 
 using namespace std;
+
+// Array is a continuous object in memory, nowhere in memory is writen its length.
+// int a[] = { 1, 2, 3 };
+// int *ap = a;
+// Array access `a[i]` is same as `*(ap + i)`, types of `a` and `ap` are different. Array access dereferencing.
+// *(ap + i) == *(((void*) ap) + sizeof(int) * i);
+// Only for pointers and integer indexes:
+// a[j] == *(&a[0]+j) == *(a+j) == *(j+a) == j[a]
+// 3["Texas"] == "Texas"[3] == 'a'
 
 void foo_taking_an_array_of_unknown_size(int a[]) {
     // Sizeof on array function parameter will return size of 'int *' instead of 'int []' 
@@ -37,11 +47,11 @@ int main() {
     {
         // size of array is a part of its type, if it is not a function argument and size is not declared
         int a[] = {1, 2, 3}; // array initializer to the left from =, number of elements is "deduced"
-        cout << "type of array contains size: " << typeid(a).name() << endl; // int [3]
-        cout << "sizeof a is " << sizeof(a) << " bytes" << endl; // 12
-        cout << "number of elements in a is " << sizeof(a)/sizeof(int) << endl;
+        cout << "type of array `a` with size is `" << describe_type(typeid(a).name()) << "`" << endl; // int [3]
+        cout << "sizeof `a` is " << sizeof(a) << " bytes" << endl; // 12
+        cout << "number of elements in `a` is " << sizeof(a)/sizeof(int) << endl;
 
-        cout << "print a's elements:";
+        cout << "print `a`'s elements:";
         for (const int x: a) {
             cout << " " << x;
         }
@@ -53,12 +63,86 @@ int main() {
         COMPILATION_ERROR(
             a = b; // error : no array assignment exists in C++
         );
+        int *ap = a;
+        int *bp = b;
+        ap = bp; // ok, pointer assignment, compiler watch declaration of variables: `a` is an array, whereas `ap` is a pointer to int.
     }
     {
         int a[1];
         COMPILATION_ERROR(
             int b[1] = a; // error : cant copy array, no implicit array copy in C++
         );
+    }
+    {
+        int a[] = {};
+        int* int_p = nullptr;
+
+        COMPILATION_ERROR(
+            // Array type 'int[0]' is not assignable
+            a = int_p;
+        )
+    }
+    {
+        // range-for works for arrays of known size
+        int a[10] = {};
+        for (int x: a) {
+            // OK
+        }
+
+        int s = 1;
+        int aa[s + 1];
+        aa[0] = 777;
+        aa[1] = 888;
+        cout << "begin iterating over `int aa[s]`: ";
+        // TODO how the hack this can work?
+        for (int x: aa) { // OK
+            cout << x << " "; // outputs "777 888 "
+        }
+        cout << "; end iterating." << endl;
+
+        // despite the declaration of a function parameter as arrays it is always a pointer:
+        cout << "void bar_taking_an_array_of_size_5(int a[5]) type is " << describe_type(typeid(bar_taking_an_array_of_size_5).name()) << endl;
+        // prints: void (int*)
+
+        // in this lambda `a` is an array of unknown size
+        auto foo = [](int a[]){
+            // Cannot build range expression with array function parameter 'a' since parameter with array type 'int[]' is treated as pointer type 'int *'
+            COMPILATION_ERROR(
+                for (int x: a) {}
+            )
+            int* int_p = nullptr;
+            a = int_p; // OK, `int a[]` is a function's parameter, treated as a pointer
+        };
+        foo(aa);
+
+        // yet, in this lambda `a` cannot be used in rage-for, because arrays a passed to functions only as pointers.
+        auto bar = [](int a[3]) {
+            // Cannot build range expression with array function parameter 'a' since parameter with array type 'int[3]' is treated as pointer type 'int *'
+            COMPILATION_ERROR(
+                for (int x: a) {}
+            )
+            int* int_p = nullptr;
+            a = int_p; // OK, `int a[]` is a function's parameter, treated as a pointer
+        };
+        bar(aa); // `aa` is `int aa[1]`, no size checks
+
+        // range-for does not work for pointers
+        int *ap = a;
+        COMPILATION_ERROR(
+            // Invalid range expression of type 'int *'; no viable 'begin' function available
+            for (int x: ap) {}
+        )
+
+        auto baz = [](array<int, 3> ar) {
+            for (int x: ar) {} // OK
+            auto [a, y, z] = ar; // OK
+        };
+        baz(array<int, 3>{ 1, 2 });
+
+        COMPILATION_ERROR(
+            // No matching function for call to object of type 'void (*)(array<int, 2>)'
+            baz(array<int, 2>{ 1, 2 });
+        )
     }
     // TODO std::array
     // TODO std::valarray
@@ -69,7 +153,7 @@ int main() {
     }
     {
         int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        foo_taking_an_array_of_unknown_size(a); // function param is declared as int[] - size of the a is lost
+        foo_taking_an_array_of_unknown_size(a); // function param is declared as int[] - size of the `a` is lost
         // compiler does not check sizes of arrays when passing one as function argument
         bar_taking_an_array_of_size_5(a); // function param is declared as int[5] - size is smaller, then size of the passed argument
     }
